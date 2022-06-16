000b627a99fc9d0af5a1973db2e11336f7e9096a
reizist <reizist@gmail.com>
2022-06-16 16:20:19 +0900

    Add vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,63 @@
+resource "aws_vpc" "this" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  instance_tenancy     = "default"
+
+  tags = {
+    Name        = local.environment
+    Product     = local.product
+    Environment = local.environment
+  }
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.0.024"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"        = "public_a"
+    "Product"     = local.product
+    "Environment" = local.environment
+  }
+}
+
+resource "aws_subnet" "public_c" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"        = "public_c"
+    "Product"     = local.product
+    "Environment" = local.environment
+  }
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    "Name"        = "private_a"
+    "Product"     = local.product
+    "Environment" = local.environment
+  }
+}
+
+
+resource "aws_subnet" "private_c" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    "Name"        = "private_c"
+    "Product"     = local.product
+    "Environment" = local.environment
+  }
+}
