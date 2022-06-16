a1c564274a3d3d9816094232f4669dc645c55111
reizist <reizist@gmail.com>
2022-06-16 16:07:34 +0900

    Add security_group.tf

---
+++ terraform/security_group.tf
@@ -0,0 +1,53 @@
+resource "aws_security_group" "elb_sg" {
+  name        = "elb"
+  description = "elb"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    description      = "HTTP From Anywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web"
+  description = "web"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    description      = "HTTP From LB"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    security_groups = [aws_security_group.elb_sg.id]
+  }
+
+  tags = {
+    Name = "allow_http_from_elb"
+  }
+}
+
+resource "aws_security_group" "db_sg" {
+  name        = "db"
+  description = "db"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    description      = "HTTP From Anywhere"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    security_groups      = [aws_security_group.web_sg.id]
+  }
+
+  tags = {
+    Name = "allow_db_from_web"
+  }
+}
