a8e4d4d8d733ec2e31b96e59f2043c6957ce9403
reizist <reizist@gmail.com>
2022-06-16 20:47:52 +0900

    Fix rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,11 +1,12 @@
 resource "aws_db_instance" "default" {
+	identifier = "${local.product}-${local.environment}"
   allocated_storage    = 20
   engine               = "mysql"
   engine_version       = "8.0.28"
   instance_class       = "db.t3.micro"
   db_name              = "rds"
   username             = "admin"
-  password             = "admin"
+  password             = "Dummy12345" # TODO: You NEED to change db password
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 	db_subnet_group_name =  aws_db_subnet_group.db_subnet.name
